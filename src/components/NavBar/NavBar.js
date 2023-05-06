99ea36236ffb1794c9e751c829b88cd3964ab2d1
capnemo20 <z.sujon20@outlook.com>
2023-05-06 12:16:36 +0100

    51-5 Create Nav toggle and navbar animation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,20 +7,20 @@ const NavBar = () => {
     const [open,setOpen] = useState(false);
     const routes = [
     { id: 1, name: "Home", path: "/home" },
-    { id: 2, name: "Products", path: "/home" },
-    { id: 3, name: "Orders", path: "/home" },
-    { id: 4, name: "Contact", path: "/home" },
-    { id: 5, name: "About", path: "/home" },
+    { id: 2, name: "Products", path: "/products" },
+    { id: 3, name: "Orders", path: "/orders" },
+    { id: 4, name: "Contact", path: "/contact" },
+    { id: 5, name: "About", path: "/about" },
   ];
   return (
-    <nav>
+    <nav className="bg-purple-200 w-full">
         {
-            open?<XMarkIcon onClick={()=>setOpen(!open)} className="h-6 w-6" />:
+            open?<XMarkIcon onClick={()=>setOpen(!open)} className="h-6 w-6 md:hidden" />:
             <Bars3Icon onClick={()=>setOpen(!open)} className="h-6 w-6" />
         }
       
       <span>{open?"open": "close"}</span>
-      <ul className="md:flex justify-center">
+      <ul className={`md:flex bg-purple-200 justify-center w-full absolute duration-500 ease-in md:static ${ open?  "top-6": "top-[-120px]"}`}>
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
